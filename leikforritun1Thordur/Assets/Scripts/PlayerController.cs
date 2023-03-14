fa90b937ac4702f6dcc93d355eebe881d01664fb
Þórður Pálmi <thordurpalmi2005@gmail.com>
2023-03-14 11:45:43 +0000

    uiAndCollatables

--- leikforritun1Thordur/Assets/Scripts/PlayerController.cs
+++ leikforritun1Thordur/Assets/Scripts/PlayerController.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0f;
 
+    public TextMeshProUGUI countText;
+    public GameObject winTetxObject;
+
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTetxObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -24,10 +32,26 @@ public class PlayerController : MonoBehaviour
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
+    
+    void SetCountText(){
+        countText.text = "Count: " + count.ToString();
+        if(count >= 8){
+            winTetxObject.SetActive(true);
+        }
+    }
 
     void FixedUpdate(){
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("PickUp")){
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+
     }
 }
