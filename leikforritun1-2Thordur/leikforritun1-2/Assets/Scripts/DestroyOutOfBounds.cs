31cf349e6c42d6bf701d65ea76e737e2cfdf125d
Þórður Pálmi <thordurpalmi2005@gmail.com>
2023-03-15 18:36:33 +0000

    simple comments

--- leikforritun1-2Thordur/leikforritun1-2/Assets/Scripts/DestroyOutOfBounds.cs
+++ leikforritun1-2Thordur/leikforritun1-2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -16,12 +16,15 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //ef maturinn kemst out of bounds
         if (transform.position.z > topDestroyLength)
         {
             Destroy(gameObject);
         }
+        //ef animal kemst out of bounds
         else if (transform.position.z < bottomDestroyLength)
         {
+            //skrifa að leikurinn endar og eyðinlegg objectið sem fór out of bounds
             Debug.Log("Game Over!");
             Destroy(gameObject);
         }
