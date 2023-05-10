e902f4e8509a80eec01133ee3d969feb18b4c1e9
Þórður Pálmi <thordurpalmi2005@gmail.com>
2023-05-10 21:52:25 +0000

    sdfg

--- LeikforritunLokaThordur/Lokaverk/Assets/Lokaverk/NewScripts/RubyController.cs
+++ LeikforritunLokaThordur/Lokaverk/Assets/Lokaverk/NewScripts/RubyController.cs
@@ -3,63 +3,43 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    // ========= MOVEMENT =================
     public float speed = 4;
-    
-    // ======== HEALTH ==========
+    public float jumpForce = 5f;   // add a new variable for jump force
     public int maxHealth = 5;
     public float timeInvincible = 2.0f;
     public Transform respawnPosition;
     public ParticleSystem hitParticle;
-    
-    // ======== PROJECTILE ==========
     public GameObject projectilePrefab;
-
-    // ======== AUDIO ==========
     public AudioClip hitSound;
     public AudioClip shootingSound;
-    
-    // ======== HEALTH ==========
     public int health
     {
         get { return currentHealth; }
     }
-    
-    // =========== MOVEMENT ==============
+
     Rigidbody2D rigidbody2d;
     Vector2 currentInput;
-    
-    // ======== HEALTH ==========
     int currentHealth;
     float invincibleTimer;
     bool isInvincible;
-   
-    // ==== ANIMATION =====
     Animator animator;
     Vector2 lookDirection = new Vector2(1, 0);
-    
-    // ================= SOUNDS =======================
     AudioSource audioSource;
-    
+
+    // Add a ground check
+    public bool isGrounded;
+
     void Start()
     {
-        // =========== MOVEMENT ==============
         rigidbody2d = GetComponent<Rigidbody2D>();
-                
-        // ======== HEALTH ==========
         invincibleTimer = -1.0f;
         currentHealth = maxHealth;
-        
-        // ==== ANIMATION =====
         animator = GetComponent<Animator>();
-        
-        // ==== AUDIO =====
         audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        // ================= HEALTH ====================
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
@@ -67,13 +47,11 @@ public class RubyController : MonoBehaviour
                 isInvincible = false;
         }
 
-        // ============== MOVEMENT ======================
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-                
+        float vertical = 0f;
         Vector2 move = new Vector2(horizontal, vertical);
-        
-        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+
+        if (!Mathf.Approximately(move.x, 0.0f))
         {
             lookDirection.Set(move.x, move.y);
             lookDirection.Normalize();
@@ -81,19 +59,12 @@ public class RubyController : MonoBehaviour
 
         currentInput = move;
 
-
-        // ============== ANIMATION =======================
-
         animator.SetFloat("Look X", lookDirection.x);
-        animator.SetFloat("Look Y", lookDirection.y);
         animator.SetFloat("Speed", move.magnitude);
-
-        // ============== PROJECTILE ======================
 
         if (Input.GetKeyDown(KeyCode.C))
             LaunchProjectile();
-        
-        // ======== DIALOGUE ==========
+
         if (Input.GetKeyDown(KeyCode.X))
         {
             RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, 1 << LayerMask.NameToLayer("NPC"));
@@ -103,46 +74,49 @@ public class RubyController : MonoBehaviour
                 if (character != null)
                 {
                     character.DisplayDialog();
-                }  
+                }
             }
         }
- 
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            transform.position += Vector3.up * jumpForce;
+            isGrounded = false;
+        }
     }
 
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        
         position = position + currentInput * speed * Time.deltaTime;
-        
         rigidbody2d.MovePosition(position);
+
     }
 
-    // ===================== HEALTH ==================
     public void ChangeHealth(int amount)
     {
         if (amount < 0)
-        { 
+        {
             if (isInvincible)
                 return;
-            
+
             isInvincible = true;
             invincibleTimer = timeInvincible;
-            
+
             animator.SetTrigger("Hit");
             audioSource.PlayOneShot(hitSound);
 
             Instantiate(hitParticle, transform.position + Vector3.up * 0.5f, Quaternion.identity);
         }
-        
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        
-        if(currentHealth == 0)
+
+        if (currentHealth == 0)
             Respawn();
-        
+
         UIHealthBar.Instance.SetValue(currentHealth / (float)maxHealth);
     }
-    
+
     void Respawn()
     {
         ChangeHealth(maxHealth);
@@ -167,5 +141,12 @@ public class RubyController : MonoBehaviour
     public void PlaySound(AudioClip clip)
     {
         audioSource.PlayOneShot(clip);
+    }
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
